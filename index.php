67462c85fe98e2b15006ab4f4081d9fedfd130f6
Fa <ngomfa@gmail.com>
2018-11-26 15:21:33 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Partie4_Exercice 2</title>
+</head>
+<!-- Faire une fonction qui prend en paramètre une chaine de caractères et qui retourne cette même chaine. -->
+<body>
+  <!-- revoir correction !!!! -->
+  <?php
+          function chaine($string = 'Ceci est une première chaine de caractère'){
+            return $string;
+          }
+          echo chaine();
+          ?>
+  </body>
+</html>
